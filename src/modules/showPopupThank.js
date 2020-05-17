217505436b52b8e25b883b8e134e5be8b22bf3eb
Сергей Худяков <sergei.hudakov@gmail.com>
2020-05-18 00:00:13 +0300

    task20

--- src/modules/showPopupThank.js
+++ src/modules/showPopupThank.js
@@ -2,6 +2,20 @@ const showPopupThank = () => {
     const   popupThank = document.querySelector('.popup-thank');
 
     popupThank.style.visibility = 'visible';
+
+    popupThank.addEventListener('click', (e) => {
+        const target = e.target;
+        const   closeBtn = target.closest('.close'),
+                popupDialog = target.closest('.feedback-wrap.popup-thank-bg');
+
+        if(closeBtn || !popupDialog){
+            popupThank.style.visibility = 'hidden';
+        }
+    });
+
+    setTimeout(() => {
+        popupThank.style.visibility = 'hidden';
+    }, 3000)
 };
 
 export default showPopupThank;
